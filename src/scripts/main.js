333dde3ee8a8e8f82c8dc2966e3889a88434ded3
Veronika D <donets.veronika@yahoo.com>
2019-10-22 23:50:22 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('hello mate academy!');
+function setCounter() {
+  if (!sessionStorage.hasOwnProperty('clickAmount')) {
+    sessionStorage.setItem('clickAmount', 0);
+  }
+
+  document.getElementById('add-text').innerHTML
+    = `You clicked ${sessionStorage.clickAmount} times`;
+};
+
+setCounter();
+
+document.body.addEventListener('click', (targetEvent) => {
+  if (!sessionStorage.hasOwnProperty('clickAmount')) {
+    sessionStorage.setItem('clickAmount', 0);
+  }
+
+  sessionStorage.clickAmount++;
+  document.getElementById('add-text').innerHTML
+    = `You clicked ${sessionStorage.clickAmount} times`;
+});
