89fe41da0732258fdd658cc316959729fd2983ad
Veronika D <donets.veronika@yahoo.com>
2019-10-23 16:33:51 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,7 @@ const text = document.getElementById('add-text');
 text.innerHTML = `You clicked 0 times`;
 
 document.body.addEventListener('click', () => {
-  sessionStorage.setItem('clickAmount', ++counter);
-  text.innerHTML = `You clicked ${sessionStorage.clickAmount} times`;
+  counter++;
+  sessionStorage.setItem('clickAmount', counter);
+  text.innerHTML = `You clicked ${counter} times`;
 });
