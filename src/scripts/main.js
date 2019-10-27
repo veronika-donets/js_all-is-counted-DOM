33041accf0daba15238559a59c5de9de5afd31b3
Veronika <54468116+veronika-donets@users.noreply.github.com>
2019-10-27 10:49:41 +0200

    Update src/scripts/main.js
    Co-Authored-By: Остап П'ятковський <44294945+gestap@users.noreply.github.com>

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@
 
 let counter = sessionStorage.getItem('clickAmount') || 0;
 const text = document.getElementById('add-text');
-text.innerHTML = `You clicked 0 times`;
+text.innerHTML = `You clicked ${counter} times`;
 
 document.body.addEventListener('click', () => {
   counter++;
